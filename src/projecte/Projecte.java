7158191045899d936c5e3cced2e39ab95a7a17b2
marc <marc@marc-B85M-D3H>
2016-10-27 09:57:15 +0200

    1r jugador

--- src/projecte/Projecte.java
+++ src/projecte/Projecte.java
@@ -16,20 +16,18 @@ public class Projecte {
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
         // TODO code application logic here
-        String nom=null, posicio; 
-        int dorsal=0, anys=0; 
-        float pes=0;
-        boolean esMasculi=false, prova=false;
-        
-        
+        String nom = null, posicio;
+        int dorsal = 0, anys = 0;
+        float pes = 0;
+        boolean esMasculi = false, omplit = false;
+
         Scanner entrada = new Scanner(System.in);
-        
+
         int opcio;
         do {
-        System.out.println("0. Sortir del programa");
+            System.out.println("0. Sortir del programa");
             System.out.println("1. Introduir un nou jugador");
             System.out.println("2. Borrar un jugador");
             System.out.println("3. Modificar jugador");
@@ -37,32 +35,52 @@ public class Projecte {
             opcio = entrada.nextInt();
             switch (opcio) {
                 case 0:
-                    
+
                     //sortir
                     break;
                 case 1:
-                   
+                    if (omplit = true) {
+                        System.out.println("Introdueix el nom del jugador");
+                        nom = entrada.nextLine();
+                        System.out.println("Introdueix la seva posició");
+                        posicio = entrada.nextLine();
+                        System.out.println("Introdueix el dorsal del jugador");
+                        dorsal = entrada.nextInt();
+                        System.out.println("Introdueix els anys del jugador");
+                        anys = entrada.nextInt();
+                        System.out.println("Introdueix el pes del jugador");
+                        pes = entrada.nextFloat();
+                        System.out.println("És del futbol Masculí? (True o False)");
+                        esMasculi = entrada.nextBoolean();
+                        System.out.println("Dades introduides correctament!");
+                        omplit = true;
+                        
+                    }
+                    
+                    if (omplit = false) {
+                        System.out.println("Les dades ja han estat introduïdes, si en vols posar més l'hauràs de borrar primer.");
+                    }
+
                     //introduir metode per donar alta 1 jugador
                     break;
                 case 2:
-                   
+
                     //introduir un metode per borrar jugadors
                     break;
                 case 3:
-                    
+
                     //Modificar jugador
                     break;
-                    
+
                 case 4:
-                    
+
                     //Llistar jugador
                     break;
-               default:
-                System.out.println("No has introduit cap numero correcte");
-            } 
-                
-        
-    }while (opcio != 0);   
-        
-} 
+                default:
+                    System.out.println("No has introduit cap numero correcte");
+            }
+
+        } while (opcio != 0);
+
+    }
 }
